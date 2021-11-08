c6541a258b3091495c491df298a77522892e754e
rafaelmendes1 <rarafafa3245@gmail.com>
2021-11-08 11:15:47 -0300

    create project

---
+++ microservices-project/src/main/java/com/github/rafaelmendes1/microservicesproject/MicroservicesProjectApplication.java
@@ -0,0 +1,13 @@
+package com.github.rafaelmendes1.microservicesproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesProjectApplication.class, args);
+	}
+
+}
---
+++ microservices-project/src/test/java/com/github/rafaelmendes1/microservicesproject/MicroservicesProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.rafaelmendes1.microservicesproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
